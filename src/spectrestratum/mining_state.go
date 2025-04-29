ae2c173a03b9eed22eb25f7180774b455586e76c
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-04-29 21:59:55 +0200

    change maxjobs to 256

--- src/spectrestratum/mining_state.go
+++ src/spectrestratum/mining_state.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spectre-project/spectred/app/appmessage"
 )
 
-const maxjobs = 300
+const maxjobs = 256
 
 type MiningState struct {
 	Jobs        map[uint64]*appmessage.RPCBlock
